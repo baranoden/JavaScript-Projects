07aa43a4cf1eb80ada9ed0d7bc2eb2fc9139beb0
baran öden <baranoden@gmail.com>
2021-10-18 23:56:19 +0300

    First Commit

---
+++ 30-Days-of-Code/Day-3-Intro-to-Conditional-Statements/solution.js
@@ -0,0 +1,16 @@
+function main(N) {
+    if (N % 2 != 0) {
+        console.log("Weird")
+
+    }
+    else if (N % 2 == 0 && N >= 2 && N<=5) {
+        console.log("Not Weird")
+    }
+    else if (N % 2 == 0 && N >= 6 && N<=20) {
+        console.log("Weird")
+    }
+    
+    else if (N % 2 == 0 && N >= 20) {
+        console.log("Not Weird")
+    }
+}
---
+++ Private-Clock-JS/README.md
@@ -0,0 +1,4 @@
+> Javascript Saat ve Karşılama
+# Greetings ayarlandı.
+# Saat Güncel olarak ayarlandı.
+![Screenshot](https://github.com/baranoden/JavaScript-Projects/blob/main/Private-Clock-JS/assets/screenshot.jpg)
