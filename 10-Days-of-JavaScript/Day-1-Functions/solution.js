0d84ddd69bee1c7b1969c496745308b64c86a73c
baran öden <baranoden@gmail.com>
2021-10-19 03:59:24 +0300

    First Commit

---
+++ 10-Days-of-JavaScript/Day-1-Functions/solution.js
@@ -0,0 +1,14 @@
+/*
+ * Create the function factorial here
+ */
+
+function factorial(number) {
+    var result = 1
+     for (var i=1;i<=number;i++){
+     result*=i
+    
+    
+    
+     }
+     return result}
+        factorial(4)
