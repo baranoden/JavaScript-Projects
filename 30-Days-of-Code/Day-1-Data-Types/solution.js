4ab01fc3e8e0daaf29ed7628b01c4c3c1f9a8694
baran öden <baranoden@gmail.com>
2021-10-19 03:41:20 +0300

    First Commit

---
+++ 30-Days-of-Code/Day-1-Data-Types/solution.js
@@ -0,0 +1,19 @@
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    // Declare second integer, double, and String variables.
+let secondInteger = +(readLine())
+let double = +(readLine())
+let string = readLine()
+    // Read and save an integer, double, and String to your variables.
+
+    // Print the sum of both integer variables on a new line.
+console.log(secondInteger + i)
+    // Print the sum of the double variables on a new line.
+console.log((double + d).toFixed(1))
+    // Concatenate and print the String variables on a new line
+console.log(s + string)
+    // The 's' variable above should be printed first.
+
+}
