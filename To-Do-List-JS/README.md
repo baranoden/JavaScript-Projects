72cb5677e5b50f7bad80637844a6a3d169726427
baran öden <baranoden@gmail.com>
2021-10-22 01:11:01 +0300

    Second Commit -Add LocaleStorage

--- To-Do-List-JS/README.md
+++ To-Do-List-JS/README.md
@@ -1,6 +1,6 @@
 # SIMPLE JAVASCRIPT TO-DO LIST
-> ADDING
-> CHECKING
-> REMOVING
-> TOAST NOTIFICATIONS
+# ADDING Items
+# CHECKING Items
+# REMOVING Items
+# TOAST NOTIFICATIONS
 ![Review](https://github.com/baranoden/JavaScript-Projects/blob/master/To-Do-List-JS/assets/screenshot.jpg)
--- To-Do-List-JS/app.js
+++ To-Do-List-JS/app.js
@@ -1,43 +1,36 @@
+
+
 let taskDOM = document.getElementById("task")
 let addDOM = document.getElementById("liveToastBtn")
 let listDOM = document.getElementById("list")
 
+
+loadedPage()
+
 // To-do işlemlerine tik atma
 var list = document.querySelector('ul')
-list.addEventListener('click', function(check) {
-  if (check.target.tagName === 'LI') {
-    check.target.classList.toggle('checked')
-  }
-})
-// To-do işlemlerini silme (var olan)
-var close = document.getElementsByClassName("close")
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement.remove()
+list.addEventListener('click', function (check) {
+    if (check.target.tagName === 'LI') {
+        check.target.classList.toggle('checked')
     }
-}
+})
 // To-do işlemi ekleme
 addDOM.addEventListener("click", () => {
     if (taskDOM.value) {
+        loadStorage(taskDOM.value)
         const listItem = document.createElement("li")
-        const deleteItem = document.createElement("a")
-        deleteItem.classList.add("close")
+        const valueItem = document.createElement('span');
+        const deleteItem = document.createElement("span")
+        deleteItem.classList.add("close");
         deleteItem.innerHTML = "X"
-        listItem.innerHTML = taskDOM.value
+        valueItem.classList.add('value');
+        valueItem.innerHTML = taskDOM.value
+        listItem.append(valueItem);
         listItem.append(deleteItem)
         listDOM.append(listItem)
         taskDOM.value = ""
 
-
-        // To-do işlemlerini silme (yeni eklenen)
-        var i;
-        for (i = 0; i < close.length; i++) {
-            close[i].onclick = function () {
-                var div = this.parentElement.remove()
-
-            }
-        }
+        deleteItem.onclick = function (e) { deleteByElement(e); }
         $(".success").toast("show");
     } else {
         $(".error").toast("show");
@@ -45,7 +38,66 @@ addDOM.addEventListener("click", () => {
     }
 })
 
+function deleteByElement(e) {
+    let parent = e.target.parentElement;
+    console.log(parent);
+    let value = parent.getElementsByClassName('value')[0].textContent.trim();
+    dltStorage(value);
+    parent.remove()
+}
+
+// Local kayıt
+function loadStorage(prm) {
+    let str = JSON.parse(localStorage.getItem("todo"))
+    let toDos;
+    if (str == null) {
+        toDos = []
+    } else {
+        toDos = getStorage()
+    }
+    toDos.push(prm)
+    localStorage.setItem("todo", JSON.stringify(toDos))
+}
+
+function getStorage() {
+    let toDo = JSON.parse(localStorage.getItem("todo"))
+    return toDo;
+}
+
+// Kayıt Getir
+
+function loadedPage() {
+    let toDo = getStorage();
+    console.log(toDo);
+    if (toDo != null) {
+        let html
+        for (let i = 0; i < toDo.length; i++) {
+            html = `<li>
+            <span class="value">${toDo[i]}</span>
+            <span class="close">X</span>
+            </li>`
+            listDOM.innerHTML += html
+        }
+    }
+}
 
 
+// Kayıt sil
+function dltStorage(prm) {
+    let toDo = getStorage()
+    toDo.forEach((element, id) => {
+        if (element === prm) {
+            toDo.splice(id, 1);
+        }
+    })
+    localStorage.setItem("todo", JSON.stringify(toDo))
+}
 
-
+(function() {
+    // To-do işlemlerini silme (var olan)
+    var close = document.getElementsByClassName("close")
+    var i;
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = (e) => deleteByElement(e);
+    }
+})();
