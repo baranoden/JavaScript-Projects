6eceee504344a07214ac3ead2e43f4e2fdb7a578
Baran Öden <baranoden@gmail.com>
2021-12-27 22:13:54 +0300

    First Commit

---
+++ 10-Days-of-JavaScript/Day-7-Regular-Expressions-III/solution.js
@@ -0,0 +1,15 @@
+// Regular expressions are most fun to solve but like i said before
+// hackerrank explainations are making the problem even harder -_-
+
+function regexVar() {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match ALL occurrences of numbers in a string.
+   */
+  const re = /[0-9]+/g;
+
+  /*
+   * Do not remove the return statement
+   */
+  return re;
+}
